e33a258480c2e0ea1c127e37853ce104afe59443
Mykhailo Dorokhin <adjorno@gmail.com>
2020-06-18 11:58:09 +0200

    Update today to 2020-06-20
    * make today date configurable
    * comment all scheduled tasks

--- src/main/java/com/adjorno/billib/rest/Schedule.java
+++ src/main/java/com/adjorno/billib/rest/Schedule.java
@@ -37,36 +37,36 @@ public class Schedule {
     @Transactional
     @Scheduled(cron = "0 0 0 * * *", zone = Ex.TIME_ZONE_CRON_PACIFIC_AUCKLAND)
     void updateDb() {
-        System.out.println("START UPDATING DB");
-        UpdateResult theUpdateResult = mUpdateController.updateDB();
-        List<Pair<String, Integer>> theChartUpdates = theUpdateResult.getChartUpdates();
-        if (Ex.isNotEmpty(theChartUpdates)) {
-            for (Pair<String, Integer> theChartUpdate : theChartUpdates) {
-                System.out.println(theChartUpdate.getFirst() + " " + UpdateResult.toString(theChartUpdate.getSecond()));
-            }
-        } else {
-            System.out.println("THERE ARE NO ANY UPDATES");
-        }
-        Date theUpdateWeek = theUpdateResult.getUpdateWeek();
-        if (theUpdateWeek != null) {
-            mTrendsController.generateTrends(BB.CHART_DATE_FORMAT.format(theUpdateWeek), TrendType.TYPE_ALL);
-            System.out.println("TRENDS CREATED");
-        }
-        System.out.println("FINISH UPDATING DB");
+//        System.out.println("START UPDATING DB");
+//        UpdateResult theUpdateResult = mUpdateController.updateDB();
+//        List<Pair<String, Integer>> theChartUpdates = theUpdateResult.getChartUpdates();
+//        if (Ex.isNotEmpty(theChartUpdates)) {
+//            for (Pair<String, Integer> theChartUpdate : theChartUpdates) {
+//                System.out.println(theChartUpdate.getFirst() + " " + UpdateResult.toString(theChartUpdate.getSecond()));
+//            }
+//        } else {
+//            System.out.println("THERE ARE NO ANY UPDATES");
+//        }
+//        Date theUpdateWeek = theUpdateResult.getUpdateWeek();
+//        if (theUpdateWeek != null) {
+//            mTrendsController.generateTrends(BB.CHART_DATE_FORMAT.format(theUpdateWeek), TrendType.TYPE_ALL);
+//            System.out.println("TRENDS CREATED");
+//        }
+//        System.out.println("FINISH UPDATING DB");
     }
 
     @Scheduled(cron = "0 20 0 * * *", zone = Ex.TIME_ZONE_CRON_PACIFIC_AUCKLAND)
     void updateDayTrack() throws ParseException {
-        System.out.println("START UPDATING DAY TRACK");
-        String theDate = PACIFIC_DATE_FORMAT.format(new Date());
-        Track theTrack = mTrackController.updateDayTrack(theDate);
-        System.out.println(theDate + " " + theTrack.getId() + " " + theTrack);
-        System.out.println("FINISH UPDATING DAY TRACK");
+//        System.out.println("START UPDATING DAY TRACK");
+//        String theDate = PACIFIC_DATE_FORMAT.format(new Date());
+//        Track theTrack = mTrackController.updateDayTrack(theDate);
+//        System.out.println(theDate + " " + theTrack.getId() + " " + theTrack);
+//        System.out.println("FINISH UPDATING DAY TRACK");
     }
 
     @Scheduled(fixedRate = 1000 * 60 * 15)
     void pingApp() {
-        System.out.println("PING APP");
-        //new RestTemplate().getForObject("https://billibrest.herokuapp.com", String.class);
+//        System.out.println("PING APP");
+//        new RestTemplate().getForObject("https://billibrest.herokuapp.com", String.class);
     }
 }
